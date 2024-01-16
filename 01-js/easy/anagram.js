7c0aa833ed54fe88d23e272dc257a03abe9e062c
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2024-01-16 23:26:00 +0530

    Week 1 anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,10 @@
 */
 
 function isAnagram(str1, str2) {
+  let str11 = str1.replace(/\s/g, "").toLowerCase().split("").sort().join("");
+  let str22 = str2.replace(/\s/g, "").toLowerCase().split("").sort().join("");
 
+  return str11 === str22;
 }
 
 module.exports = isAnagram;
