94a1aef0590f2fc1db1093cf25d4b95614b0a4ea
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2024-02-28 14:38:10 +0530

    week5 BusinessCard done

--- week-5/level-1/BusinessCard/src/App.jsx
+++ week-5/level-1/BusinessCard/src/App.jsx
@@ -2,7 +2,24 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="main_container">
+      <div className="container">
+        <h2>Ajay Dewangan</h2>
+        <p>A MERN Stack Developer.</p>
+
+        <h4>Interests</h4>
+        <span>Data science and algorithm</span>
+        <br />
+        <span>Backend Development</span>
+        <br />
+        <span>Front End Development</span>
+        <br />
+        <br />
+        <div>
+          <button><a href="https://github.com/ajaydewangan1100">Linkedin</a></button>&nbsp;&nbsp;
+          <button><a href="https://github.com/ajaydewangan1100">Twitter</a></button>
+        </div>
+      </div>
     </div>
   );
 }
